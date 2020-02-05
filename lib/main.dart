8ab430f6b42d3ab3ee733b4ccb08b2083d1b7afa
Tudor Tanasciuc <tudor.tanasciuc@nagarro.com>
2020-02-05 09:57:46 +0200

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 
 import 'package:flutter/material.dart';
-import 'package:nagarro_cash_app/User_interface/Pages/expenses.dart';
 import 'package:nagarro_cash_app/User_interface/Pages/home_screen.dart';
 import 'package:nagarro_cash_app/User_interface/Pages/intro_screen.dart';
 import 'package:nagarro_cash_app/User_interface/Pages/loading_screen.dart';
